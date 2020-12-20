f29ac057c9ad9f75bc7746a1cf17ca9de0fc0a9f
NextStop-Las-Vegas <rabackoff94@icloud.com>
2020-12-21 03:41:51 +0500

    add extension for class Figure

--- first.playground/Contents.swift
+++ first.playground/Contents.swift
@@ -5,8 +5,34 @@ class Figure {
     var cornerRadius: Int { return 0 }
 }
 
-protocol PerimeterQuadrangularFigures {
+//MARK: -HOMEWORK 1.2 EXTENSIONS
 
+extension Figure {
+    
+    func perimeter(width: Int, length: Int) -> Int{
+        return 2*(width + length)}
+}
+
+extension Figure {
+    
+    func perimeterIsEven(width: Int, length: Int) -> Bool{
+        var isEven: Bool{
+            return (perimeter(width: width, length: length)) % 2 == 0 ? true : false
+        }
+        return isEven
+    }
+}
+
+extension Figure {
+    
+    func square(width: Int, length: Int) -> Int{
+        return width * length}
+}
+
+//MARK: -
+
+protocol PerimeterQuadrangularFigures {
+    
     var width : Double { get set }
     var length : Double { get set }
 }
@@ -18,7 +44,7 @@ extension PerimeterQuadrangularFigures {
 class Traingle : Figure {
     override var name : String { return "trinagle" }
     override var cornerRadius: Int { return 0 }
-
+    
     func square(lengthOne: Int, lengthTwo: Int, lengthThree: Int) -> Int{
         return lengthOne*lengthTwo*lengthThree
     }
@@ -27,7 +53,7 @@ class Traingle : Figure {
 class Rectangle : Figure {
     override var name : String { return "rectangle" }
     override var cornerRadius: Int { return 0 }
-
+    
     func square(lengthOne: Int, lengthTwo: Int) -> Int{
         return lengthOne*lengthTwo
     }
@@ -36,7 +62,7 @@ class Rectangle : Figure {
 class Circle : Figure {
     override var name : String { return "circle" }
     override var cornerRadius: Int { return 0 }
-
+    
     func square(radius: Float) -> Float{
         let pi: Float = 3.1415
         var square: Float = 0
@@ -46,23 +72,22 @@ class Circle : Figure {
 }
 
 class SecondRectangle : PerimeterQuadrangularFigures {
-
+    
     var width: Double = 0.0
     var length: Double = 0.0
-
+    
 }
 
 class Square : PerimeterQuadrangularFigures {
-
+    
     var width: Double = 0.0
     var length: Double = 0.0
-
+    
 }
 
 class Parallelogram : PerimeterQuadrangularFigures {
-
+    
     var width: Double = 0.0
     var length: Double = 0.0
-
+    
 }
-
